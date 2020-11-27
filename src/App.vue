4a626a58679619932eebd2ebbd59e4ee30daaa5d
andrey1025 <62780077+andrey1025@users.noreply.github.com>
2020-11-27 18:06:35 +0300

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,104 +1,112 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    
-    <h1>Employees</h1>
-    <employee-form @add:employee="addEmployee" />
-    <employee-table :employees="employees" @delete:employee="deleteEmployee" @edit:employee="editEmployee"/>
+    <div class="container">
+      <div class="form-group row">
+        <label class="control-label col-sm-3 page-title text-left" for="username"><strong>Github</strong> <i>Search</i></label>
+        <div class="col-sm-9 input-group">
+          <input type="text" class="form-control" id="username" placeholder="Enter github username" v-model="username" v-on:keyup="enterKey">
+          <div class="input-group-append">
+            <button class="btn btn-pink" type="submit">
+              <i class="fa fa-search"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="search-result">
+        <div class="empty-user" v-if="isEmpty">
+          <h1 class="pink">User not found :(</h1>
+        </div>
+      </div>
+      <user_data :info="info" :repos="repos" v-if="showUserDetail && !isEmpty"/>
+    </div>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import EmployeeTable from './components/EmployeeTable.vue'
-import EmployeeForm from './components/EmployeeForm.vue'
-
-
+import user_data from './components/UserDetail.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld,
-    EmployeeTable,
-    EmployeeForm
+    user_data
   },
   data() {
     return {
-      employees: [],
+      username : "",
+      info:{},
+      repos : [],
+      isEmpty:false,
+      showUserDetail:false
     }
   },
-  mounted() {
-    this.getEmployees()
-  },
   methods:{
-    async addEmployee(employee) {
+    async searchGithubUser(){
+      var userInfoUrl = "https://api.github.com/users/" + this.username
+      var userReposUrl = "https://api.github.com/users/" + this.username + "/repos"
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users', {
-          method: 'POST',
-          body: JSON.stringify(employee),
+        const userInfo = await fetch(userInfoUrl, {
+          method: 'GET',
           headers: { 'Content-type': 'application/json; charset=UTF-8' },
         })
-        const data = await response.json()
-        this.employees = [...this.employees, data]
+        const data = await userInfo.json()
+        if(data.avatar_url){
+          this.info = data
+          const userRepos = await fetch(userReposUrl, {
+            method: 'GET',
+            headers: { 'Content-type': 'application/vnd.github.mercy-preview+json; charset=UTF-8' },
+          })
+          const repos = await userRepos.json()
+          this.repos = repos
+          this.showUserDetail = true
+          this.isEmpty = false
+        } else{
+          this.isEmpty = true
+        }
       } catch (error) {
         console.error(error)
       }
     },
-    async deleteEmployee(id) {
-      try {
-        await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
-          method: "DELETE"
-        });
-        this.employees = this.employees.filter(employee => employee.id !== id);
-      } catch (error) {
-        console.error(error);
+    enterKey:function(e){
+      if(e.keyCode == 13){
+        this.searchGithubUser()
       }
-    },
-    async editEmployee(id, updatedEmployee) {
-      try {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
-          method: 'PUT',
-          body: JSON.stringify(updatedEmployee),
-          headers: { 'Content-type': 'application/json; charset=UTF-8' },
-        })
-        const data = await response.json()
-        this.employees = this.employees.map(employee => (employee.id === id ? data : employee))
-      } catch (error) {
-        console.error(error)
-      }
-    },
-    async getEmployees() {
-      try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users')
-        const data = await response.json()
-        this.employees = data
-      } catch (error) {
-        console.error(error)
-      }
-    }, 
-  },
- 
-
+    }
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
-
-button {
-  background: #009435;
-  border: 1px solid #009435;
+.search-result{
+  margin:30px 0px;
 }
-
-.small-container {
-  max-width: 680px;
+.pink{
+  color:#ac53f2;
+}
+.page-title{
+  margin-bottom: 0px;
+}
+.page-title strong{
+  font-size: 30px;
+  margin-right: 10px;
+}
+.page-title i{
+  font-size: 30px;
+}
+.input-group #username{
+  height:100%;
+}
+.input-group-append button{
+  padding:0px 30px;
+  color:white;
+}
+.btn-pink{
+  background-color: #ac53f2 !important;
 }
 </style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="user_data" class="row" v-if="info">
+    <div class="col-ls-3 col-md-3 col-sm-3">
+      <img :src="info.avatar_url" class="avatar"/>
+      <div class="text-left user-detail">
+        <p class="name"><a v-bind:href="info.html_url">{{ info.name }}</a></p>
+        <p class="username">{{ info.login }}</p>
+        <p class="extra"><i class="fa fa-empire"></i><span>{{ info.bio }}</span></p>
+        <p class="extra"><i class="fa fa-dribbble	"></i><span>{{ info.company }}</span></p>
+        <p class="extra"><i class="fa fa-star-o"></i><span>{{ info.following }}</span></p>
+        <p class="extra"><i class="fa fa-cube"></i><span>{{ info.public_repos }}</span></p>
+        <p class="extra"><i class="fa fa-users"></i><span>{{ info.followers }}</span></p>
+      </div>
+    </div>
+    <div class="col-ls-9 col-md-9 col-sm-9">
+      <div v-for="repo in repos" :key="repo.id" class="repo text-left">
+        <a v-bind:href="repo.clone_url" class="pink">{{ repo.name }}</a>
+        <p>{{ repo.description != null ? repo.description : "No description"}}</p>
+        <p><i class="fa fa-star-o"></i> {{ repo.watchers_count }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'user_data',
+    props: {
+      info: Object,
+      repos:Array
+    },
+    mounted(){
+      console.log(this.info)
+      console.log(this.repos)  
+    }
+  }
+</script>
+
+<style scoped>
+  .avatar{
+    width:100%;
+  }
+  .user-detail p{
+    margin:5px 0px;
+  }
+  .name{
+    font-size: 25px;
+    font-weight: 500;
+  }
+  .name a{
+    color:#2f2f2f;
+  }
+  .username{
+    font-weight: 300;
+    margin-bottom: 20px;
+  }
+  .extra i{
+    font-size: 20px;
+    margin-right: 10px;
+  }
+  .repo{
+    margin-bottom: 15px;
+  }
+  .repo p{
+    margin-bottom: 5px;
+  }
+  .repo a{
+    font-size: 20px;
+  }
+</style>
